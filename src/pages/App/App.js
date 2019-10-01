103f782ffb220b86ce0130156f25e9962f719ffa
Andrew Theismann <andrew@theismannconsulting.com>
2019-10-01 17:09:33 -0500

    complete

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from '../../components/Map/Map';
-import {getCurrentLatLng} from '../../services/geolocation';
+import { getCurrentLatLng } from '../../services/geolocation';
+import { getCurWeatherByLatLng } from '../../services/weather-api';
 
 class App extends Component {
   state = {
     lat: null,
     lng: null,
+    temp: null,
+    icon: '',
   };
 
   async componentDidMount() {
     const {lat,lng} = await getCurrentLatLng();
-    this.setState({lat, lng});
+    const weatherData = await getCurWeatherByLatLng(lat, lng);
+    this.setState({
+      lat, 
+      lng,
+      temp: Math.round(weatherData.main.temp),
+      icon: weatherData.weather[0].icon,
+    });
+    console.log(Math.round(weatherData.main.temp));
   };
   render() {
     return (
@@ -20,7 +30,16 @@ class App extends Component {
           lat={this.state.lat}
           lng={this.state.lng}
         />
-        <header className='App-header'>REACT WEATHER</header>
+        <header className='App-header'>
+        <div>{this.state.temp}&deg;</div>
+          REACT WEATHER
+          {this.state.icon && 
+            <img
+              src={`https://openweathermap.org/img/w/${this.state.icon}.png`}
+              alt='Current Conditions'
+            />
+          }
+        </header>
       </div>
     );
   };
