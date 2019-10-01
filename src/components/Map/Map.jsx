48828140ec73e1eb58dad1d453af3797d79af298
Andrew Theismann <andrew@theismannconsulting.com>
2019-10-01 15:29:32 -0500

    add map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,25 +2,37 @@ import React from 'react';
 import styles from './Map.module.css';
 import mapStyle from './map-style';
 
-function Map({lat, lng, zoom}) {
-  // Create a ref object that exposes native DOM elements
-  const mapDiv = React.createRef();
+class Map extends React.Component {
+  mapDiv = React.createRef();
 
-  if (lat && lng) {
-    const map = new window.google.maps.Map(
-      mapDiv.current, {
-        zoom: zoom || 12,
-        center: {lat, lng},
-        disableDefaultUI: true,
-        styles: mapStyle
-      }
+  setMap() {
+    if (this.props.lat && this.props.lng) {
+      const {lat, lng, zoom} = this.props;
+      const map = new window.google.maps.Map(
+        this.mapDiv.current, {
+          zoom: zoom || 12,
+          center: {lat, lng},
+          disableDefaultUI: true,
+          styles: mapStyle
+        }
+      );
+      new window.google.maps.Marker({position: {lat, lng}, map: map});
+    };
+  };
+
+  componentDidMount(){
+    this.setMap();
+  };
+
+  componentDidUpdate(){
+    this.setMap();
+  };
+
+  render() {
+    return (
+      <div ref={this.mapDiv} className={styles.Map}></div>
     );
-    new window.google.maps.Marker({position: {lat, lng}, map: map});
-  }
-
-  return (
-    <div ref={mapDiv} className={styles.Map}></div>
-  );
-}
+  };
+};
 
 export default Map;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from '../../components/Map/Map';
+import {getCurrentLatLng} from '../../services/geolocation';
 
 class App extends Component {
+  state = {
+    lat: null,
+    lng: null,
+  };
 
+  async componentDidMount() {
+    const {lat,lng} = await getCurrentLatLng();
+    this.setState({lat, lng});
+  };
   render() {
     return (
       <div className='App'>
-        <Map />
+        <Map
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
         <header className='App-header'>REACT WEATHER</header>
       </div>
     );
-  }
-
-}
+  };
+};
 
 export default App;
